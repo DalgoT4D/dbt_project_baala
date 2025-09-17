709a117f5da658dfccaf4b619b8d7b835f47fca6
Siddhant Singh <ssiddhant3030@gmail.com>
2025-09-17 18:46:45 +0530

    new runs

--- models/intermediate/int_msi_cs_2024_baseline_survey.sql
+++ models/intermediate/int_msi_cs_2024_baseline_survey.sql
@@ -10,15 +10,22 @@
 
 with transformed as (
     select
-        -- All fields from staging (includes flattened JSONB data)
-        *
+        -- All fields except Airbyte and data quality columns
+        {{ 
+            dbt_utils.star(
+                from=ref('stg_msi_cs_2024_baseline_survey'), 
+                except=[
+                    "_airbyte_raw_id", "_airbyte_extracted_at", "_airbyte_meta",
+                    "has_json_data", "has_submission_time", "has_end_time"
+                ]
+            ) 
+        }}
         
     from {{ ref('stg_msi_cs_2024_baseline_survey') }}
     
     -- Add any business logic, filtering, or transformations here
     -- For example:
     -- where survey_id is not null
-    -- and has_json_data = true
 )
 
 select 
